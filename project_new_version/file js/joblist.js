e8ba15738eabeb4b43e59b0a31e5a88381587cab
Srey.Sokhom <41099990+SreySokhom@users.noreply.github.com>
2019-01-29 23:02:21 +0700

    add company list

--- project_new_version/file js/joblist.js
+++ project_new_version/file js/joblist.js
@@ -11,11 +11,16 @@
    };
    firebase.initializeApp(config);
 
+   //create a variable point to root of database in firebase
    const root = firebase.database().ref().child("Job");
+
+   //use on() to get data 
    root.on("child_added", snap => {
      var position = snap.child('position').val();
      var company = snap.child('company').val();
     var img=snap.child('img').val();
+    
+    //diplay data from database in firebase to div 
      $("#div1").append(`	<div class="col-xl-4 col-12"   id="div1">
               <div class="row div2"  id="di1">
               <div class="col-3" id="di2"><img src="${img}"></div>
