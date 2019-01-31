0507d47cd51eac2fe6de736c333ab9033b8d3c72
shurricanex <sarakkornsakol@gmail.com>
2019-01-31 16:04:26 +0700

    pop-up modal  added into joblist.js

--- public/file js/joblist.js
+++ public/file js/joblist.js
@@ -27,9 +27,43 @@
 								<div class="col-3 ">
 									<img src="${img}">
 								</div>
-								<div class="col-9">
-									<a href="#">${position}</a><br><span>${company}</span>
-								</div>
+								 <div class="col-9">
+									<a href="#" data-toggle="modal" data-target=".bd-example-modal-xl" >${position}</a><br><span>${company}</span>
+
+
+
+									<div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+										<div class="modal-dialog modal-xl">
+											<div class="modal-content">
+											<div class="modal-header">
+											<h3 class="modal-title" id="exampleModalLongTitle">${img},${company}</h3>
+											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+												<span aria-hidden="true">&times;</span>
+											</button>
+										</div>
+										<div class="modal-body">
+											<h4>Position : ${position}</h4> 
+											<div><h5>location</h5>#3 Boeng Prolit 7makara </div>
+											<div><h5>Job Description<h5></div>
+											<p>
+											Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam facilis pariatur tenetur est praesentium eius. <br> Totam fuga inventore veniam deleniti quisquam nisi facere perferendis delectus deserunt, quidem sequi eius et a tempore maiores laborum sit id <br>dignissimos ea dicta. Dolore a quidem necessitatibus voluptates officiis commodi labore pariatur minima voluptas magnam et hic eum, aliquid dolorem officia nesciunt? <br>Obcaecati culpa veniam totam voluptatum? Saepe modi optio voluptatum temporibus, fugiat atque ipsa iusto, assumenda eveniet consequuntur laborum perspiciatis quos aliquam.<br> Eum ipsa maxime fugiat corporis quisquam animi, sunt mollitia earum veniam minima quis officia repellat, dignissimos facere saepe vero repudiandae ullam? Hic nisi voluptatum doloremque, enim numquam maxime,<br> aliquid repudiandae magnam dolorem deserunt error et harum veniam vitae, 
+											</p>
+											<h5> Requirement <h5>
+											<p>
+											rem nulla cumque vero alias eum. Qui ab nesciunt facere et ut libero harum fuga sapiente sunt cupiditate nisi recusandae pariatur, consequuntur doloribus dicta, illum sequi accusamus distinctio rem? Doloremque, libero tenetur suscipit velit ex adipisci consequuntur excepturi. <br>Facere ad quo, dolorem dolor, similique quae ullam voluptates nemo hic quisquam autem? Quasi doloribus necessitatibus quidem quae ipsum suscipit ratione enim, rem distinctio. Delectus aliquid, debitis maiores rem quasi, quas tempore tempora fugiat veritatis pariatur enim dignissimos,<br> totam dolore voluptatem at iste corrupti voluptatibus!
+											</p>
+										</div>
+										<div class="modal-footer">
+											<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+											<button type="button" class="btn btn-primary">Apply</button>
+										</div>
+											</div>
+										</div>
+									</div>
+									
+
+
+									</div>
 							</div>
 						</div>
 					</div>				
