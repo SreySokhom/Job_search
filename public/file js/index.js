15cb9997214c64d555a83204f5bfc8c5e4e0a68d
shurricanex <sarakkornsakol@gmail.com>
2019-02-17 21:04:08 +0700

    update

--- public/file js/index.js
+++ public/file js/index.js
@@ -7,154 +7,154 @@ var config = {
     messagingSenderId: "920751215712"
   };
   firebase.initializeApp(config);
-  arrayObject = [];
+//   arrayObject = [];
     
-  function search (inputedString){
-        firebase.database().ref().child("Job").on("child_added" , snap=>{
-            // console.log(snap.val().position);
-           arrayObject.push(snap.val());
-           var i;
-           for( i=0;i<arrayObject.length; i++){
-            if(arrayObject.position.match(inputedString)){
-                console.log(arrayObject.position.match(inputedString));
-            }
-           }
-           console.log(arrayObject)
-            console.log("You have fetched data.")
-        })
- }
-search("Teacher");
-console.log(arrayObject.length);
-
-
-const root=firebase.database().ref().child("Job");
-//  function accountingShow(){
-//      root.on("child_added", snap=>{
-//        var acc=[];
-//      //  var index=[];
-//       acc.push(snap.val());
-//      if(snap.val().position=="Teacher"){
-//        $("#jobcategory").append(` <div class="col-xl-6 col-md-6 col-12">
-//        <div class="div2 p-2 mb-3 mx-auto">
-//            <div class="row">
-//                <div class="col-3 ">
-//                    <img src="${snap.val().img}">
-//                </div>
-//                <div class="col-9">
-//                    <a href="#">${snap.val().company}</a><br><span>${snap.val().position}</span>
-//                </div>
-//            </div>
-//        </div>
-//    </div>	 `)
-////         console.log(acc);
-// }
-//    
-//    //   for(i=0;i<acc.length;i++){
-//    //     $("#jobcategory").append(` <div class="col-xl-6 col-md-6 col-12">
-//    //     <div class="div2 p-2 mb-3 mx-auto">
-//    //         <div class="row">
-//    //             <div class="col-3 ">
-//    //                 <img src="${snap.val("img")}">
-//    //             </div>
-//    //             <div class="col-9">
-//    //                 <a href="#">${snap.val("company")}</a><br><span>${snap.val("position")}</span>
-//    //             </div>
-//    //         </div>
-//    //     </div>
-//    // </div>	 `)
-//    //   }
-//     
-//       
-//      })
+//   function search (inputedString){
+//         firebase.database().ref().child("Job").on("child_added" , snap=>{
+//             // console.log(snap.val().position);
+//            arrayObject.push(snap.val());
+//            var i;
+//            for( i=0;i<arrayObject.length; i++){
+//             if(arrayObject.position.match(inputedString)){
+//                 console.log(arrayObject.position.match(inputedString));
+//             }
+//            }
+//            console.log(arrayObject)
+//             console.log("You have fetched data.")
+//         })
 //  }
-
-		
-
-
-  (function(){
-	//   to display jobcatagery
-      root.on("child_added",snap=>{
-        //var allData=[];
-        $("#jobcategory").append(` <div class="col-xl-6 col-md-6 col-12">
-        <div class="div2 p-2 mb-3 mx-auto">
-            <div class="row">
-                <div class="col-3 ">
-                    <img src="${snap.val().img}">
-                </div>
-                <div class="col-9">
-                    <a href="#">${snap.val().company}</a><br><span>${snap.val().position}</span>
-                </div>
-            </div>
-        </div>
-    </div>	 `)
-      })
-	  		//   to display only accounting job
-		root.on("child_added", snap=>{
-			
-			if(snap.val().position=="Accounting " || snap.val().position=="Various Positiion"){
-				$("#accounting").append(` <div class="col-xl-6 col-md-6 col-12">
-				<div class="div2 p-2 mb-3 mx-auto">
-					<div class="row">
-						<div class="col-3 ">
-							<img src="${snap.val().img}">
-						</div>
-						<div class="col-9">
-							<a href="#">${snap.val().company}</a><br><span>${snap.val().position}</span>
-						</div>
-					</div>
-				</div>
-			</div>	 `)
-			}
-		})
-				//display only advisory job
-				root.on("child_added", snap=>{
-					var acc=[];
-				  //  var index=[];
-				   acc.push(snap.val());
-				  if(snap.val().position=="Teacher"){
-					  console.log(snap.val.position);
-					$("#education").append(` <div class="col-xl-6 col-md-6 col-12">
-					<div class="div2 p-2 mb-3 mx-auto">
-						<div class="row">
-							<div class="col-3 ">
-								<img src="${snap.val().img}">
-							</div>
-							<div class="col-9">
-								<a href="#">${snap.val().company}</a><br><span>${snap.val().position}</span>
-							</div>
-						</div>
-					</div>
-				</div>	 `)
-					 }
-					})
-  }());
+// search("Teacher");
+// console.log(arrayObject.length);
 
 
-//jquery code
-$(document).ready(function(){
-			$('#bar').click(function(){
-	//			var sub_nav=$('.sub_nav');
-				$('.sub_nav').toggle();
-			});
-//			to hide accounting job
-			$('#accounting').hide();
-			$('#education').hide();
+//  function accountingShow(){
+    //      root.on("child_added", snap=>{
+        //        var acc=[];
+        //      //  var index=[];
+        //       acc.push(snap.val());
+        //      if(snap.val().position=="Teacher"){
+            //        $("#jobcategory").append(` <div class="col-xl-6 col-md-6 col-12">
+            //        <div class="div2 p-2 mb-3 mx-auto">
+            //            <div class="row">
+            //                <div class="col-3 ">
+            //                    <img src="${snap.val().img}">
+            //                </div>
+            //                <div class="col-9">
+            //                    <a href="#">${snap.val().company}</a><br><span>${snap.val().position}</span>
+            //                </div>
+            //            </div>
+            //        </div>
+            //    </div>	 `)
+            ////         console.log(acc);
+            // }
+            //    
+            //    //   for(i=0;i<acc.length;i++){
+                //    //     $("#jobcategory").append(` <div class="col-xl-6 col-md-6 col-12">
+                //    //     <div class="div2 p-2 mb-3 mx-auto">
+                //    //         <div class="row">
+                //    //             <div class="col-3 ">
+                //    //                 <img src="${snap.val("img")}">
+                //    //             </div>
+                //    //             <div class="col-9">
+                //    //                 <a href="#">${snap.val("company")}</a><br><span>${snap.val("position")}</span>
+                //    //             </div>
+                //    //         </div>
+                //    //     </div>
+                //    // </div>	 `)
+                //    //   }
+                //     
+                //       
+                //      })
+                //  }
+                
+                
+                const root=firebase.database().ref().child("Job");
 
-			$('#account').click(function(){
-				$('#jobcategory').hide();
-				$('#education').hide();
-				$('#accounting').show();
-			});
-//			to display all job back
-			$('#alljob').click(function(){
-				$('#accounting').hide();
-				$('#education').hide();
-				$('#jobcategory').show();
-			});
-			//to diplay edu job
-			$('#educat').click(function(){
-				$('#accounting').hide();
-				$('#jobcategory').hide();
-				$('#education').show();
-			});
-	});
+
+                (function(){
+                    //   to display jobcatagery
+                      root.on("child_added",snap=>{
+                        //var allData=[];
+                        $("#jobcategory").append(` <div class="col-xl-6 col-md-6 col-12">
+                        <div class="div2 p-2 mb-3 mx-auto">
+                            <div class="row">
+                                <div class="col-3 ">
+                                    <img src="${snap.val().img}">
+                                </div>
+                                <div class="col-9">
+                                    <a href="#">${snap.val().company}</a><br><span>${snap.val().position}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>	 `)
+                      })
+                              //   to display only accounting job
+                        root.on("child_added", snap=>{
+                            
+                            if(snap.val().position=="Accounting " || snap.val().position=="Various Positiion"){
+                                $("#accounting").append(` <div class="col-xl-6 col-md-6 col-12">
+                                <div class="div2 p-2 mb-3 mx-auto">
+                                    <div class="row">
+                                        <div class="col-3 ">
+                                            <img src="${snap.val().img}">
+                                        </div>
+                                        <div class="col-9">
+                                            <a href="#">${snap.val().company}</a><br><span>${snap.val().position}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>	 `)
+                            }
+                        })
+                                //display only advisory job
+                                root.on("child_added", snap=>{
+                                    var acc=[];
+                                  //  var index=[];
+                                   acc.push(snap.val());
+                                  if(snap.val().position=="Teacher"){
+                                      console.log(snap.val.position);
+                                    $("#education").append(` <div class="col-xl-6 col-md-6 col-12">
+                                    <div class="div2 p-2 mb-3 mx-auto">
+                                        <div class="row">
+                                            <div class="col-3 ">
+                                                <img src="${snap.val().img}">
+                                            </div>
+                                            <div class="col-9">
+                                                <a href="#">${snap.val().company}</a><br><span>${snap.val().position}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>	 `)
+                                     }
+                                    })
+                  }());
+                
+                
+                //jquery code
+                $(document).ready(function(){
+                            $('#bar').click(function(){
+                    //			var sub_nav=$('.sub_nav');
+                                $('.sub_nav').toggle();
+                            });
+                //			to hide accounting job
+                            $('#accounting').hide();
+                            $('#education').hide();
+                
+                            $('#account').click(function(){
+                                $('#jobcategory').hide();
+                                $('#education').hide();
+                                $('#accounting').show();
+                            });
+                //			to display all job back
+                            $('#alljob').click(function(){
+                                $('#accounting').hide();
+                                $('#education').hide();
+                                $('#jobcategory').show();
+                            });
+                            //to diplay edu job
+                            $('#educat').click(function(){
+                                $('#accounting').hide();
+                                $('#jobcategory').hide();
+                                $('#education').show();
+                            });
+                    });
